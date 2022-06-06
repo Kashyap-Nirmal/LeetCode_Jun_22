a3e58a8647d36680b30285c40bc76deaf9bec5cf
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2022-06-06 20:49:11 +0530

    Time: 43 ms (5.04%), Space: 6.9 MB (98.89%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -2,8 +2,8 @@
 class Solution {
 public:
     string reverseWords(string s) {    
-        cout<<"\n\n";
-        cout<<s.size()<<"\n";
+        //cout<<"\n\n";
+        //cout<<s.size()<<"\n";
         reverse(s.begin(),s.end());
         int i,j=0;
         string::iterator it,pit;
@@ -12,7 +12,7 @@ public:
         for(i=0;i<s.size()-1;i=(it-s.begin()))
         {
             it = find(s.begin()+(pit-s.begin()), s.end(), ' ');        
-            cout<<it-s.begin()<<" "<<pit-s.begin()<<"  ";
+            //cout<<it-s.begin()<<" "<<pit-s.begin()<<"  ";
             if(pit==it)
             {
                 s.erase(s.begin()+(it-s.begin()));
@@ -23,14 +23,14 @@ public:
                 reverse(s.begin()+(pit-s.begin()),s.begin()+(it-s.begin()));
                 pit = s.begin()+(it-s.begin())+1;
             }                
-            cout<<" "<<pit-s.begin()<<" "<<"\n";
+            //cout<<" "<<pit-s.begin()<<" "<<"\n";
         }
         it = find(s.begin()+(pit-s.begin()), s.end(), ' ');     
         //cout<<"O "<<s.size()<<" "<<it-s.begin()<<" "<<pit-s.begin()<<" "<<"\n";       
         reverse(s.begin()+(pit-s.begin()),s.begin()+(it-s.begin()));
         while(*s.rbegin()==' ')
         {
-            cout<<"I ";
+            //cout<<"I ";
             s.erase(s.begin()+s.size()-1);                        
         }            
         return s;
