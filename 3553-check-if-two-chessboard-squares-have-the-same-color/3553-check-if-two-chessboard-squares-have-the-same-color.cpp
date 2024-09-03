3a94cb6b31b23074063b758b4c7e3d73151b56f8
Kashyap-Nirmal <kashyapnirmal18@gmail.com>
2024-09-04 01:40:26 +0530

    Updated readme. Delete duplicate directory.

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool checkTwoChessboards(string coordinate1, string coordinate2) {
-        // Sometimes it will result in negative difference. So to get positive difference we added 8.
-        if((8 + coordinate1[0] - coordinate2[0]) % 2  == (8 + coordinate1[1] - coordinate2[1]) % 2) return true;
-        else return false;
-    }
-};
