55aa260ba57ee75ef66b48ae790eb1bd50fed56e
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2024-09-10 01:22:11 +0530

    Time: 0 ms (100%), Space: 9 MB (96.78%) - LeetHub

---
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++)
+            for (int j = 0; j < s.length(); j++) 
+                if (s[i] == t[j]) {
+                    ans += abs(i - j);
+                    break;
+                }
+        return ans;
+    }
+};
