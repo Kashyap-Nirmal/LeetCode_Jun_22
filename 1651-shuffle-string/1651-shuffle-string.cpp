eef2b229d7ec1bdd0cacdd4d795d2d56632f6cc7
Kashyap-Nirmal <kashyapnirmal18@gmail.com>
2024-10-02 01:11:50 +0530

    Time: 9 ms (26.84%), Space: 18.3 MB (73.2%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    string restoreString(string s, vector<int>& indices) {
-        string sCopy = s;
-        for(int i = 0; i < s.length(); i++) sCopy[indices[i]] = s[i];
-        return sCopy;
-    }
-};
