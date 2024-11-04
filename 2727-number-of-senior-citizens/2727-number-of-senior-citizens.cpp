3343b465c43f97f5fd6af4829b8d4f6900bbc444
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2024-11-04 23:29:08 +0530

    Time: 0 ms (100%), Space: 17.6 MB (48.7%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt = 0;
+        for (int i = 0; i < details.size(); i++) 
+            if (details[i][11] > '6' || (details[i][11] >= '6' && details[i][12] > '0')) cnt++;
+        return cnt;
+    }
+};
