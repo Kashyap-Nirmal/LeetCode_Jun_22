c5bf350c3f9cf5b138ae65e0df9539a0c88462f8
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2024-11-15 00:29:06 +0530

    Time: 3 ms (9.3%), Space: 10.4 MB (32.99%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.cpp
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.cpp
@@ -7,9 +7,9 @@ public:
             if (m.find(nums[i]) == m.end()) m.insert(pair<int, int>(nums[i], 1));
             else m[nums[i]]++;
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (m[nums[i]] > 1) continue;
-            else sum += nums[i];
+        for (auto it: m) {
+            if (it.second > 1) continue;
+            else sum += it.first;
         }
         return sum;
     }
