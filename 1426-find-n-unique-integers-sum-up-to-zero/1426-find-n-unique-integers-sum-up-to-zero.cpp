2693be34461d6d58a8ddce86b86e05bfbbdbfe20
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2024-12-07 00:51:32 +0530

    Time: 0 ms (100%), Space: 9.7 MB (6.63%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> vec (n, 0);
-        int j = -1 * n / 2;
-        for (int i = 0; i < n; i++, j++) {
-            if (n % 2 == 1 && i == n / 2) vec[i] = 0;
-            else if (i == n / 2) {
-                j++;
-                vec[i] = j;
-            } else vec[i] = j;
+        for (int i = 0, j = -1 * n / 2; i < n; i++, j++) {
+            if (i == n / 2 && n % 2 == 0) j++;
+            vec[i] = j;
         }
         return vec;
     }
