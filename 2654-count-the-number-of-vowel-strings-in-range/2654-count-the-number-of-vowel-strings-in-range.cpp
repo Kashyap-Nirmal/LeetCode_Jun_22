7ea546a2f83bd459438ffdfc098a5f87625802b0
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2024-12-21 18:36:44 +0530

    Time: 0 ms (100%), Space: 36.8 MB (26.23%) - LeetHub

--- 2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.cpp
+++ 2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool isVowel(char c) {
-        string s(1, c);
-        regex b("[aeiou]");
-        return regex_match(s, b);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
     int vowelStrings(vector<string>& words, int left, int right) {
         int cnt = 0;
-        for(int i = left; i <= right; i++) {
+        for(int i = left; i <= right; i++)
             if (isVowel(words[i][0]) && isVowel(words[i][words[i].size() - 1])) cnt++;
-        }      
         return cnt;  
     }
 };
