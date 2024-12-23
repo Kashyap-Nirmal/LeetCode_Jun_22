744ffc98b243421bd9ce59e372bdf5ab015d3fb1
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2024-12-24 00:09:08 +0530

    Time: 65 ms (60.38%), Space: 9.7 MB (63.61%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -5,9 +5,9 @@ public:
         for (int i = low; i <= high; i++) {
             int left = 0, right = 0;
             string temp = to_string(i);
-            for (int j = 0; temp.size() % 2 == 0 && j < temp.size(); j++) {
-                if (j < temp.size() / 2) left += temp[j];
-                else right += temp[j];
+            for (int j = 0; temp.size() % 2 == 0 && j < temp.size() / 2; j++) {
+                left += temp[j];
+                right += temp[temp.size() / 2 + j];
             }
             if (left != 0 && left == right) cnt++;
         }
