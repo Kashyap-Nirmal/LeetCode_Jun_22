f9e3c4674f62fd2b3b8a773978e94623cad240ab
Kashyap Nirmal <kashyapnirmal18@gmail.com>
2025-02-18 01:41:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (47.37%) - LeetHub

--- 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.cpp
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.cpp
@@ -1,14 +1,32 @@
+
 class Solution {
+
 public:
+
     int countEven(int num) {
+
         int cnt = 0;
+
         if (num >= 2) cnt = 1;
+
         for (int i = 4; i <= num; i++) {
-            string stri = to_string(i);
+
             int temp = 0;
-            for(int j = 0; j < stri.size(); temp += (stri[j] - '0'), j++);
+
+            temp += i % 10;
+
+            temp += (i / 10);
+
+            temp += (i / 100);
+
+            temp += (i / 1000);
+
             if (temp % 2 == 0) cnt++;            
+
         }
+
         return cnt;
+
     }
+
 };
